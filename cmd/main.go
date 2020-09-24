0d080e3f0b279c4bbb4f527d03519f17476c3535
Paul Wrubel <pwrubel7@gmail.com>
2020-09-23 22:23:25 -0500

    added some stuff to test deployments

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Printf("Hey, it works!\n")
+	envVar, isSet := os.LookupEnv("PRINT_THIS_VAR")
+	if isSet {
+		fmt.Printf("You can find your requested env var [PRINT_THIS_VAR] here: %s\n", envVar)
+	} else {
+		fmt.Printf("You blew it! Your requested env var [PRINT_THIS_VAR] has not been set!\n")
+	}
 }
