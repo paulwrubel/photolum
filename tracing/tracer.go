eceaec825e424043b3dd6174c5cf14ba9ea81c7b
Paul Wrubel <pwrubel7@gmail.com>
2020-09-24 14:11:31 -0500

    added sample image to generate

--- tracing/tracer.go
+++ tracing/tracer.go
@@ -2,6 +2,8 @@ package tracing
 
 import (
 	"image"
+	"image/color"
+	"math"
 
 	"github.com/google/uuid"
 	"github.com/paulwrubel/photolum/persistence"
@@ -13,6 +15,16 @@ func SaveImage(sceneID uuid.UUID) error {
 		return err
 	}
 	newImage := image.NewRGBA64(image.Rect(0, 0, scene.ImageWidth, scene.ImageHeight))
+	for y := 0; y < scene.ImageHeight; y++ {
+		for x := 0; x < scene.ImageWidth; x++ {
+			col := color.RGBA64{
+				R: uint16(0.0 * float64(math.MaxUint16)),
+				G: uint16((float64(x) / float64(scene.ImageWidth)) * float64(math.MaxUint16)),
+				B: uint16((float64(y) / float64(scene.ImageHeight)) * float64(math.MaxUint16)),
+				A: uint16(1.0 * float64(math.MaxUint16))}
+			newImage.SetRGBA64(x, y, col)
+		}
+	}
 	persistence.UpdateImage(sceneID, newImage)
 	return nil
 }
