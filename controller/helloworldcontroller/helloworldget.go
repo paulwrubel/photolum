9150698befed63eb14928fbf109afe851797831a
Paul Wrubel <pwrubel7@gmail.com>
2020-10-03 19:58:12 -0500

    rearranged

--- controller/helloworldcontroller/helloworldget.go
+++ controller/helloworldcontroller/helloworldget.go
@@ -24,12 +24,12 @@ func GetHandler(response http.ResponseWriter, request *http.Request, baseLog *lo
 	})
 	log.Debug("request received")
 
-	response.WriteHeader(http.StatusOK)
-	response.Header().Add("Content-Type", "application/json")
 	helloWorldResponse := GetResponse{
 		Message: "Hello, World!",
 	}
 	json.NewEncoder(response).Encode(helloWorldResponse)
+	response.WriteHeader(http.StatusOK)
+	response.Header().Add("Content-Type", "application/json")
 
 	log.Debug("request completed")
 }
