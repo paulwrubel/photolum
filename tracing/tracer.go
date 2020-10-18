c1f6983ac73962e125a7da82ac546817c63c5f5c
Paul Wrubel <pwrubel7@gmail.com>
2020-10-18 01:05:40 -0500

    who needs semaphores anyways, right?

--- tracing/tracer.go
+++ tracing/tracer.go
@@ -111,7 +111,8 @@ func traceRound(params *config.Parameters,
 	roundNum int,
 	tileChan chan<- bool) {
 
-	sem := semaphore.NewWeighted(int64(runtime.NumCPU()))
+	//sem := semaphore.NewWeighted(int64(runtime.NumCPU()))
+	sem := semaphore.NewWeighted(int64(len(tiles)))
 
 	wg := sync.WaitGroup{}
 	for i, tile := range tiles {
